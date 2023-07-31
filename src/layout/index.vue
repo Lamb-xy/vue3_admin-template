2340bea90928a591f5f26b32c73a6052356e80b0
peiyao.yang <peiyao.yang@qiyuesuo.com>
2023-07-31 18:00:47 +0800

    feat: 封装layout组件

---
+++ src/layout/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="layout_container">
+        <!-- 左侧菜单 -->
+        <div class="layout_slider">123</div>
+        <!-- 顶部导航 -->
+        <div class="layout_tabbar">456</div>
+        <!-- 内容展示区 -->
+        <div class="layout_main">
+            <p height="1000000px"></p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.layout_container {
+    width: 100%;
+    height: 100vh;
+    background: pink;
+    .layout_slider {
+        color: white;
+        width: $base-menu-width;
+        height: 100vh;
+        background: $base-menu-background;
+    }
+    .layout_tabbar {
+        position: fixed;
+        top: 0px;
+        left: $base-menu-width;
+        width: calc(100% - $base-menu-width);
+        height: $base-tabbar-height;
+    }
+    .layout_main {
+        position: absolute;
+        width: calc(100% - $base-menu-width);
+        height: calc(100vh - $base-tabbar-height);
+        top: $base-tabbar-height;
+        left: $base-menu-width;
+        background: rgb(109, 134, 71);
+
+        padding: 20px;
+        overflow: auto;
+    }
+}
+</style>
