9024dde35cfb0e3b9b38e10d17b5b406e63df671
peiyao.yang <peiyao.yang@qiyuesuo.com>
2023-07-31 15:59:10 +0800

    feat: 定义路由配置信息

---
+++ src/router/routes.ts
@@ -0,0 +1,27 @@
+// 引入类型校验，规范ts开发
+import { RouteRecordRaw } from 'vue-router'
+
+// constantRoute: 不需要动态判断权限的路由（静态路由）
+export const constantRoute: RouteRecordRaw[] = [
+    {
+        path: '/login',
+        component: () => import('@/views/login/index.vue'),
+        name: 'login', // 命名路由：用于权限管理
+    },
+    {
+        path: '/',
+        component: () => import('@/views/home/index.vue'),
+        name: 'home', // 命名路由：用于权限管理
+    },
+    {
+        path: '/404',
+        component: () => import('@/views/404/index.vue'),
+        name: '404', // 命名路由：用于权限管理
+    },
+    // 任意路由:以上都未匹配到
+    {
+        path: '/:pathMatch(.*)*',
+        redirect: '/404', // 重定向到404
+        name: 'Any',
+    },
+]
